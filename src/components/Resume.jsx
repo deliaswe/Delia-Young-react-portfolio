e8ac9a61d1508c8aa7628c7060fe394c4c4ee2b1
delia <you@example.com>
2023-12-01 21:27:46 -0500

    resume edit

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,7 +6,7 @@ function Resume() {
     <section id='resume'>
         <p>View my resume here</p>
         <a
-        href='https://drive.google.com/file/d/1JoU6FXMc32LxewdHJZLcSc0HEgpb7m8U/view?usp=sharing'
+        href='https://drive.google.com/file/d/1gfX8DLvUiU__3PpgGzYqKzapLSQlT3ds/view?usp=sharing'
         target='_blank'
         rel='noreferrer'
         >
