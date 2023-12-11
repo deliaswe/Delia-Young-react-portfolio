a5c0ea59828595e449644c3e8ba4e845c66ab64a
delia <you@example.com>
2023-12-11 01:03:48 -0500

    code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,7 +32,7 @@ function About() {
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col xs={12} md={10}>
+                    <Col xs={10} md={7}>
                         <Card>
                             <Card.Body>
                                 <Card.Title className='text-center mb-4'>
