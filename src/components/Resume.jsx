addbc2344f1f153bd262338d70f6f0f280a4102c
delia <you@example.com>
2024-01-11 13:32:31 -0500

    resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,7 +6,7 @@ function Resume() {
     <section id='resume'>
         <p>View my resume here</p>
         <a
-        href='https://drive.google.com/file/d/1gfX8DLvUiU__3PpgGzYqKzapLSQlT3ds/view?usp=drive_link'
+        href='https://drive.google.com/file/d/1atYlWYJgt_nWjeGCcvm74xmTBf_vib3M/view?usp=sharing'
         target='_blank'
         rel='noreferrer'
         >
